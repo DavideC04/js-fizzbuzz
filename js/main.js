25e9418fc89622858645b680966404e5aafbec93
Davide Cangiano <cangiano.0497@gmail.com>
2023-07-04 16:30:37 +0200

    Bonus 1 and 2 done. (finished)

--- js/main.js
+++ js/main.js
@@ -9,7 +9,14 @@ Prima di partire a scrivere codice poniamoci qualche domanda:
 
 Consigli del giorno:
     1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare passo dopo passo   
-    2. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano" anche su un foglietto */
+    2. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano" anche su un foglietto 
+    
+COME BONUS:
+BONUS 1
+    -Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta, così come fatto in classe questa mattina.
+
+BONUS 2
+    -Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5. */
 
 
 
@@ -36,6 +43,34 @@ for (let i = 1; i <= 100; i++) {
 
     } else {
         console.log(i);
+    }
+}
+
+
+// Stampo i quadrati con colori diversi nell'HTML, usando un altro ciclo for
+
+for (j = 1; j <= 100; j++) {
+
+    if (j % 3 == 0 && j % 5 == 0) {
+        document.getElementById("squareContainer").innerHTML += `
+      <div class="square">
+                <p class="text-center bg-danger text-light fs-5 fw-3 vertical-align-square">FrizzBuzz</p>
+            </div>`
+    } else if (j % 3 == 0) {
+        document.getElementById("squareContainer").innerHTML += `
+      <div class="square bg_multiple3">
+                <p class="text-center text-light fs-5 fw-3 vertical-align-square">Frizz</p>
+            </div>`
+    } else if (j % 5 == 0) {
+        document.getElementById("squareContainer").innerHTML += `
+      <div class="square bg_multiple5">
+                <p class="text-center text-light fs-5 fw-3 vertical-align-square">Buzz</p>
+            </div>`
+    } else {
+        document.getElementById("squareContainer").innerHTML += `
+      <div class="square bg_otherNumbers">
+                <p class="text-center text-light fs-5 fw-3 vertical-align-square">${j}</p>
+            </div>`
     }
 }
 
